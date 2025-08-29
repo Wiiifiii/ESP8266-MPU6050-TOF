6d7ae506424024d8ac51726d577d74371bb22dfc
Wefky Hamed <wefky.hamed@outlook.com>
2025-08-29 10:51:52 +0300

    Clean up, comment, and commit

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 // index.js
 import 'react-native-gesture-handler'; 
-import { registerRootComponent } from 'expo'; // ensures your App is the entry point
+import { registerRootComponent } from 'expo'; // ensures App is the entry point
 
 import App from './App';
 
